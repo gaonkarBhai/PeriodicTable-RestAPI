6ebc3e8cb52d93a9a47a770fe7fbc14aa9f50aa9
gaonkarBhai <naveengaonkar014@gmail.com>
2023-08-26 19:12:22 +0530

    nting

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // 13/4/2023
 
-const express = require("express");
+const express = require('express')
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -9,6 +9,6 @@ const periodicTable = require('../src/models/tableSchema');
 const router = require("./routers/routes");
 
 app.use(express.json());
-app.use(router)
+app.use('/api/v1',router)
 
 app.listen(PORT, () => console.log("Server On"));
